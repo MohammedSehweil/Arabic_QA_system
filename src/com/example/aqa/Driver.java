7008b10ead5ebc651763f6b4fa128f6ae01987ff
Maher <maher@souktel.org>
2017-09-18 16:41:53 +0300

    Add Java Project Structure(2)

--- src/com/example/aqa/Driver.java
+++ src/com/example/aqa/Driver.java
@@ -2,6 +2,6 @@ package com.example.aqa;
 
 public class Driver {
     public static  void main(String[] args){
-        System.out.println("HELLO");
+        System.out.println("Project Created Successfully");
     }
 }
