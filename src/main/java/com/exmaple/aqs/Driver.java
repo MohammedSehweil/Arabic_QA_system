dca68f032cf5b1c373a54427a5bd49dc3eee784a
Maher <maher@souktel.org>
2017-09-18 19:45:08 +0300

    Fix

--- src/main/java/com/exmaple/aqs/Driver.java
+++ src/main/java/com/exmaple/aqs/Driver.java
@@ -8,6 +8,6 @@ import java.io.IOException;
 public class Driver {
 
     public static void main(String[] args) throws IOException {
-
+            new DocumentRetrieval().getDoctooriLinks( "السكري", 3);
     }
 }
