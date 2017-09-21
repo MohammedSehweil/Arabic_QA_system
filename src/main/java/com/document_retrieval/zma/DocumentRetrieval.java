4b175f88a481f75628ab3384c7dc25764bfe8a9d
MaherSaleem <the-maher@live.com>
2017-09-21 13:17:25 +0300

    Add DocumentRetrival File to package

---
+++ src/main/java/com/document_retrieval/zma/DocumentRetrieval.java
@@ -0,0 +1,55 @@
+package com.document_retrieval.zma;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DocumentRetrieval {
+
+
+    private ArrayList<String> _getLinksOnePage(String searchQuery, int page, String link, String pageVariableName, String selector, String regex) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link + searchQuery + "&" + pageVariableName + "=" + page).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements newsHeadlines = doc.select(selector);
+
+        String result = newsHeadlines.toString();
+
+
+        //regex to extract links
+        Pattern urlPattern = Pattern.compile(
+                regex,
+                Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher matcher = urlPattern.matcher(result);
+
+        ArrayList<String> links = new ArrayList<String>();
+        while (matcher.find()) {
+            int matchStart = matcher.start(1);
+            int matchEnd = matcher.end();
+            links.add(result.substring(matchStart, matchEnd - 1));
+            System.out.println(result.substring(matchStart, matchEnd - 1));
+
+            // now you have the offsets of a URL match
+        }
+        return links;
+    }
+
+    /*
+        generic function to get documents links
+     */
+    public ArrayList<String> getLinks(String searchQuery, int numPages, String link,String pageVariableName ,String selector, String regex) {
+        ArrayList<String> allLinks = new ArrayList<String>();
+        for (int i = 0; i <= numPages; i++) {
+            allLinks.addAll(_getLinksOnePage(searchQuery,i,link,pageVariableName, selector, regex));
+        }
+        return allLinks;
+    }
+}
--- src/main/java/com/main/zma/Driver.java
+++ src/main/java/com/main/zma/Driver.java
@@ -1,6 +1,7 @@
 package com.main.zma;
 
 
+import com.document_retrieval.zma.DocumentRetrieval;
 
 import java.io.IOException;
 
@@ -8,5 +9,6 @@ import java.io.IOException;
 public class Driver {
 
     public static void main(String[] args) throws IOException {
+            new DocumentRetrieval().getLinks( "السكري", 3,"http://www.doctoori.net/search/?word=", "pg",".content-main a.btn-primary","href=\"([^\"]*)\"" );
     }
 }
