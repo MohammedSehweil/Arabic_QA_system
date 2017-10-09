3b1a9d9302678528e66531602b38e3a0c281149e
MaherSaleem <the-maher@live.com>
2017-10-09 22:51:36 +0300

    add code to genarate aarf file add some of initialization of weka ngram

--- src/main/java/com/AQAS/question_type/Driver.java
+++ src/main/java/com/AQAS/question_type/Driver.java
@@ -3,34 +3,66 @@ package com.AQAS.question_type;
 
 import opennlp.tools.util.StringList;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import weka.core.tokenizers.NGramTokenizer;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instances;
+
 public class Driver {
 
-    public static void main(String[] args) {
 
-        int n = 2; //n-gram
-        String[] s = "I saw the fox saw the fox".split(" ");
-        Collection<StringList> nGrams = NGramUtils.getNGrams(new StringList(s), n);
-        Collection<StringList> nGrams_cloned = NGramUtils.getNGrams(new StringList(s), n);
+    /**
+     * Generates the Instances object and outputs it in ARFF format to stdout.
+     *
+     * @param args ignored
+     * @throws Exception if generation of instances fails
+     */
+    public static void main(String[] args) throws Exception {
 
 
-        Set<StringList> hs = new HashSet<>();
-        hs.addAll(nGrams_cloned);
-        nGrams_cloned.clear();
-        nGrams_cloned.addAll(hs);
+        StringToWordVector stringToWordVector = new StringToWordVector();
+        stringToWordVector.setIDFTransform(true);
+        stringToWordVector.setTFTransform(true);
 
-//        System.out.println(nGrams);
-        System.out.println(nGrams_cloned);
-        for (StringList stringList : nGrams_cloned) {
-            if (n == 1) {
-                System.out.println(stringList.toString() + ": " + NGramUtils.calculateUnigramMLProbability(stringList.getToken(0), nGrams));
-            } else if (n == 2) {
-                System.out.println(stringList.toString() + ": " + NGramUtils.calculateNgramMLProbability(stringList, nGrams));
-            }
-        }
+        NGramTokenizer nGramTokenizer = new NGramTokenizer();
+        nGramTokenizer.setNGramMaxSize(2);
+        nGramTokenizer.setNGramMinSize(2);
+        nGramTokenizer.setDelimiters(" \n" + " \t.,;:'\"()?!");
+
+        stringToWordVector.setTokenizer(nGramTokenizer);
+
+        stringToWordVector.setAttributeIndices("review");
+        stringToWordVector.setAttributeIndices("TypeClass");
+
+//
+//        int n = 2; //n-gram
+//        String[] s = "I saw the fox saw the fox".split(" ");
+//        Collection<StringList> nGrams = NGramUtils.getNGrams(new StringList(s), n);
+//        Collection<StringList> nGrams_cloned = NGramUtils.getNGrams(new StringList(s), n);
+//
+//
+//        Set<StringList> hs = new HashSet<>();
+//        hs.addAll(nGrams_cloned);
+//        nGrams_cloned.clear();
+//        nGrams_cloned.addAll(hs);
+//
+//        System.out.println(nGrams_cloned);
+//        for (StringList stringList : nGrams_cloned) {
+//            if (n == 1) {
+//                System.out.println(stringList.toString() + ": " + NGramUtils.calculateUnigramMLProbability(stringList.getToken(0), nGrams));
+//            } else if (n == 2) {
+//                System.out.println(stringList.toString() + ": " + NGramUtils.calculateNgramMLProbability(stringList, nGrams));
+//            }
+//        }
 
     }
 }
--- src/main/java/com/AQAS/question_type/HelpersQT.java
+++ src/main/java/com/AQAS/question_type/HelpersQT.java
@@ -1,7 +1,62 @@
 package com.AQAS.question_type;
 
 
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+import java.util.ArrayList;
+
 public class HelpersQT {
 
 
+
+    public static void createAarfJava(){
+        ArrayList<Attribute> atts;
+        ArrayList<Attribute> attsRel;
+        ArrayList<String> attVals;
+        ArrayList<String> attValsRel;
+        Instances data;
+        Instances dataRel;
+        double[] vals;
+        double[] valsRel;
+        int i;
+
+        // 1. set up attributes
+        atts = new ArrayList<Attribute>();
+        // - string
+        atts.add(new Attribute("question", (ArrayList<String>) null));
+        // - nominal
+        attVals = new ArrayList<String>();
+        attVals.add("numaric");
+        attVals.add("list");
+        attVals.add("paragraph");
+        atts.add(new Attribute("QuestionType", attVals));
+
+
+        // 2. create Instances object
+        data = new Instances("MyRelation", atts, 0);
+
+        // 3. fill with data
+        // first instance
+        vals = new double[data.numAttributes()];
+        vals[0] = data.attribute(0).addStringValue("This is a string1!");
+        vals[1] = 0;
+        data.add(new DenseInstance(1.0, vals));
+
+        vals = new double[data.numAttributes()];
+        vals[0] = data.attribute(0).addStringValue("This is a string2!");
+        vals[1] = 1;
+        data.add(new DenseInstance(1.0, vals));
+
+        vals = new double[data.numAttributes()];
+        vals[0] = data.attribute(0).addStringValue("This is a strin3!");
+        vals[1] = 2;
+        data.add(new DenseInstance(1.0, vals));
+
+
+        // 4. output data
+        System.out.println(data);
+
+    }
 }
