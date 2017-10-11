b426b0768fe0cfc217c9a09f23553e7f3bf51708
MaherSaleem <the-maher@live.com>
2017-10-11 17:28:35 +0300

    fix

--- src/main/java/com/AQAS/question_type/Driver.java
+++ src/main/java/com/AQAS/question_type/Driver.java
@@ -7,7 +7,7 @@ public class Driver {
 
 
     public static void main(String[] args) throws Exception {
-        Instances ngrammedVectores = FeatureVector.initializeStringToWordVector(1, 1, ConfigQT.ARFF_FILE, "C:\\Users\\the-m\\Desktop\\maherData.arff", "output.csv");
+        Instances ngrammedVectores = FeatureVector.initializeStringToWordVector(1, 1, ConfigQT.MANUALLY_DATA,  null, "output.csv");
 
         MachineLearning.initializeClassifier(ngrammedVectores);
     }
